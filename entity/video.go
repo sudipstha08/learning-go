07a32f2be6fb9137dc53d7465e13ba8ff7c71cae
Sudip Shrestha <sudipstha08@gmail.com>
2021-02-15 10:38:21 +0545

    add: routes grouping

--- entity/video.go
+++ entity/video.go
@@ -8,7 +8,7 @@ type Person struct {
 }
 
 type Video struct {
-	Title       string `json:"title" binding:"min=2,max=10" validate:"is-cool"`
+	Title       string `json:"title" binding:"min=2,max=200" validate:"is-cool"`
 	Description string `json:"description" binding:"max=20"`
 	URL         string `json:"url" binding:"required,url"`
 	Author      Person `json:"author" binding:"required"`
