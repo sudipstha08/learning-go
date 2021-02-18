4785cddf9075e44ea4203036a039d27c96cf8844
Sudip Shrestha <sudipstha08@gmail.com>
2021-02-18 14:42:57 +0545

    fix: import issue in basics

--- basics/03_packages/grettings/grettings.go
+++ basics/03_packages/grettings/grettings.go
@@ -1,4 +1,4 @@
-package greetings
+package grettings
 
 import "fmt"
 
--- basics/03_packages/hello/main.go
+++ basics/03_packages/hello/main.go
@@ -1,12 +1,13 @@
-package main
+package hello
 
 import (
 	"fmt"
-	"learning-go/basics/03_packages/greetings"
+
+	"learning-go/basics/03_packages/grettings"
 )
 
 func main() {
 	// Get a greeting message and print it.
-	message := greetings.Hello("Gladys")
+	message := grettings.Hello("Dec")
 	fmt.Println(message)
 }
