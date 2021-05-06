ad687e7116254c735eb8d53f18ad813f50f4d2de
Sudip Shrestha <sudipstha08@gmail.com>
2021-05-06 09:14:20 +0545

    feat: interfaces

--- basics/13_interfaces/main.go
+++ basics/13_interfaces/main.go
@@ -2,41 +2,82 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
-// Define interface
-type Shape interface {
-	area() float64
+type SalaryCalculator interface {
+	CalculateSalary() int
 }
 
-type Circle struct {
-	x, y, radius float64
+type Permanent struct {
+	empId    int
+	basicpay int
+	pf       int
 }
 
-type Rectangle struct {
-	width, height float64
+type Contract struct {
+	empId    int
+	basicpay int
 }
 
-func (c Circle) area() float64 {
-	return math.Pi * c.radius * c.radius
+type Freelancer struct {
+	empId       int
+	ratePerHour int
+	totalHours  int
 }
 
-func (r Rectangle) area() float64 {
-	return r.width * r.height
+//salary of permanent employee is sum of basic pay and pf
+func (p Permanent) CalculateSalary() int {
+	return p.basicpay + p.pf
 }
 
-func getArea(s Shape) float64 {
-	return s.area()
+//salary of contract employee is the basic pay alone
+func (c Contract) CalculateSalary() int {
+	return c.basicpay
+}
+
+//salary of freelancer
+func (f Freelancer) CalculateSalary() int {
+	return f.ratePerHour * f.totalHours
+}
+
+/*
+total expense is calculated by iterating through the SalaryCalculator slice and summing
+the salaries of the individual employees
+*/
+func totalExpense(s []SalaryCalculator) {
+	expense := 0
+	for _, v := range s {
+		expense = expense + v.CalculateSalary()
+	}
+	fmt.Printf("Total Expense Per Month $%d", expense)
 }
 
 func main() {
-	circle := Circle{x: 0, y: 0, radius: 5}
-	rectangle := Rectangle{width: 10, height: 5}
+	pemp1 := Permanent{
+		empId:    1,
+		basicpay: 5000,
+		pf:       20,
+	}
+	pemp2 := Permanent{
+		empId:    2,
+		basicpay: 6000,
+		pf:       30,
+	}
+	cemp1 := Contract{
+		empId:    3,
+		basicpay: 3000,
+	}
+	freelancer1 := Freelancer{
+		empId:       4,
+		ratePerHour: 70,
+		totalHours:  120,
+	}
+	freelancer2 := Freelancer{
+		empId:       5,
+		ratePerHour: 100,
+		totalHours:  100,
+	}
+	employees := []SalaryCalculator{pemp1, pemp2, cemp1, freelancer1, freelancer2}
+	totalExpense(employees)
 
-	fmt.Printf("Circle Area: %f\n", getArea(circle))
-	fmt.Printf("Rectangle Area: %f\n", getArea(rectangle))
 }
-
-// https://www.youtube.com/watch?v=qJKQZKGZgf0
-// https://www.youtube.com/watch?v=lh_Uv2imp14&t=306s
